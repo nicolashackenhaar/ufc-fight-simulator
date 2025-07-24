507d5805280c2f8341033055cd3f82bcc6ae025e
Nicolas Hack <nicolashackenhaar@gmail.com>
2025-07-24 12:57:47 -0300

    add class Fight

---
+++ src/Fight.java
@@ -0,0 +1,25 @@
+public class Fight {
+    private Fighter fighter1;
+    private Fighter fighter2;
+
+    public Fight(Fighter fighter1, Fighter fighter2) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+    }
+
+    public Fighter simulate(Fighter fighter1, Fighter fighter2) {
+        double percent1 = this.fighter1.winningPercentage();
+        double percent2 = this.fighter2.winningPercentage();
+
+        if (percent1 > percent2) {
+            return this.fighter1;
+        } else if (percent1 < percent2) {
+            return this.fighter2;
+        }
+        return null;
+    }
+
+    public void printWinner(){
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         Fighter fighter1 = new Fighter("Jon Jones", 27, 1, 10);
         Fighter fighter2 = new Fighter("Stipe Miocic", 20, 2, 15);
 
+        Fight fight = new Fight(fighter1, fighter2);
+        Fighter winner = fight.simulate(fighter1, fighter2);
+
+
     fighter1.sheet();
+    fighter2.sheet();
+
+        System.out.println("o vencedor é: "+winner.getName());
 
 
 
